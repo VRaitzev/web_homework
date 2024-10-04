3ad8174e6c9b2a14670d8b5b3efd93cd007bae2c
VRaitzev <163679361+VRaitzev@users.noreply.github.com>
2024-10-04 23:29:01 +0300

    Add files via upload

---
+++ lab1/12/src/Component.jsx
@@ -0,0 +1,77 @@
+import uniqueId from 'lodash/uniqueId';
+import React from 'react';
+
+export default class Component extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { currentValue: 0, logList: [] };
+  }
+
+  handleButtonClick = (isPositive) => {
+    this.setState((prevState) => {
+      // Если лог пустой, начинаем с 0
+      const lastValue = prevState.logList.length > 0 
+        ? prevState.logList[0].currentValue 
+        : 0;
+
+      const newValue = isPositive ? lastValue + 1 : lastValue - 1;
+
+      const newLog = { id: uniqueId(), currentValue: newValue };
+
+      return {
+        logList: [newLog, ...prevState.logList], 
+      };
+    });
+  };
+
+  handleLogButton = (deleteId) => {
+    this.setState((prevState) => {
+      const newLogList = prevState.logList.filter((item) => item.id !== deleteId);
+
+      return {
+        logList: newLogList,
+        currentValue: newLogList.length === 0 ? 0 : prevState.currentValue, // сбрасываем, если список пуст
+      };
+    });
+  };
+
+  render() {
+    const { logList } = this.state;
+
+    return (
+      <div>
+        <div className="btn-group font-monospace" role="group">
+          <button 
+            onClick={() => this.handleButtonClick(true)} 
+            type="button" 
+            className="btn btn-outline-success"
+          >
+            +
+          </button>
+          <button 
+            onClick={() => this.handleButtonClick(false)} 
+            type="button" 
+            className="btn btn-outline-danger"
+          >
+            -
+          </button>
+        </div>
+
+        {logList.length > 0 && ( // Рендерим лог, только если есть записи
+          <div className="list-group">
+            {logList.map((logEl) => (
+              <button
+                key={logEl.id}
+                onClick={() => this.handleLogButton(logEl.id)} 
+                type="button"
+                className="list-group-item list-group-item-action"
+              >
+                {logEl.currentValue}
+              </button>
+            ))}
+          </div>
+        )}
+      </div>
+    );
+  }
+}
