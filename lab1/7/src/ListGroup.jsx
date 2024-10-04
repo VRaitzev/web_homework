637c0b4c5e4e9cc0f8cc30d6b82b058ec20d4ab7
VRaitzev <163679361+VRaitzev@users.noreply.github.com>
2024-10-04 23:24:48 +0300

    Add files via upload

---
+++ lab1/7/src/ListGroup.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Children } from 'react';
+
+// BEGIN (write your solution here)
+export default class ListGroup extends React.Component
+{
+    render(){
+    const {children} = this.props
+    return(
+        <ul className="list-group">
+            {React.Children.map(children, (child) =>
+            {
+                return<li className='list-group-item'>{child}</li>
+            })}
+        </ul>
+    )
+    }
+}
+// END
